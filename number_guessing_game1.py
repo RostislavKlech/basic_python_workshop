1c1700ab7c785f66deee735702b4307ef5a68475
RostislavKlech <KlechRostislav@seznam.cz>
2024-12-11 12:14:17 +0100

    first solution

--- number_guessing_game1.py
+++ number_guessing_game1.py
@@ -1,1 +1,18 @@
+import random
 
+guessing_number = random.randint(1, 100)
+guessed_number = 0
+print(guessing_number)
+
+while guessed_number != guessing_number:
+    try:
+        guessed_number = int(input("Guess the number: "))
+    except ValueError:
+        print("It is not a number!")
+        continue
+    if guessed_number < guessing_number:
+        print("Too small!")
+    elif guessed_number > guessing_number:
+        print("Too big!")
+    else:
+        print("You win!")
