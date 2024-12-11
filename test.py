86ff5c55dd22a3839f2ada6a10c42aec16194e69
RostislavKlech <KlechRostislav@seznam.cz>
2024-12-11 13:40:04 +0100

    second solution

--- test.py
+++
@@ -1,8 +0,0 @@
-guessed_numbers = [1, 2, 3, 8, 3, 5]
-
-for i in range(6):
-    print(i)
-    print(not isinstance(guessed_numbers[i], int))
-    print(guessed_numbers[i] not in list(range(1, 50)))
-    print(guessed_numbers[i] in guessed_numbers)
-
